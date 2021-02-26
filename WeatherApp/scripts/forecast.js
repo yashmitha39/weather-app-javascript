7d5a31b87413a4ce401f9b8c95f4b084abd3f871
yashmitha39 <77225635+yashmitha39@users.noreply.github.com>
2021-02-26 13:39:47 +0530

    Add files via upload

---
+++ WeatherApp/scripts/forecast.js
@@ -0,0 +1,20 @@
+const key = "llTGWDwm6O8MJxdjPni1b621aGtijxNf";
+
+const getWeatherConditons = async(id) => {
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+const getCity = async(city) => {
+
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
